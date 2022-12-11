68c438aa69ff59dc4edc4cb1523bf3dc5ab2dc1d
TominoFTW <behal.t7@gmail.com>
2022-12-11 23:32:33 +0100

    Taks 2 doneish

--- part02/analysis.py
+++ part02/analysis.py
@@ -14,6 +14,13 @@ import zipfile
 
 
 def load_data(filename: str) -> pd.DataFrame:
+    """
+    Loads data from a zip file and returns a pandas DataFrame.
+
+    :param filename: name of the zip file
+
+    :return: pandas DataFrame
+    """
     # tyto konstanty nemente, pomuzou vam pri nacitani
     headers = ["p1", "p36", "p37", "p2a", "weekday(p2a)", "p2b", "p6", "p7", "p8", "p9", "p10", "p11", "p12", "p13a",
                "p13b", "p13c", "p14", "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23", "p24", "p27", "p28",
@@ -45,36 +52,153 @@ def load_data(filename: str) -> pd.DataFrame:
                     if file != "CHODCI.csv":
                         tmp = pd.read_csv(zip2.open(
                             file), sep=";", header=None, names=headers, encoding="cp1250", low_memory=False)
-                        tmp["region"] = {v:k for k,v in regions.items()}.get(file.removesuffix(".csv"))
+                        tmp["region"] = {v: k for k, v in regions.items()}.get(
+                            file.removesuffix(".csv"))
                         df.append(tmp)
 
-    return pd.concat(df)
+    return pd.concat(df, ignore_index=True)
 
 # Ukol 2: zpracovani dat
 
 
 def parse_data(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
-    new_df = pd.DataFrame()
+    """
+    Parses the data and returns a pandas DataFrame.
+
+    :param df: pandas DataFrame
+    :param verbose: whether to print additional information
+
+    :return: pandas DataFrame
+    """
+    new_df = df.copy()
+    new_df = new_df.drop_duplicates(subset="p1")
+
+    new_df["date"] = pd.to_datetime(new_df["p2a"]).astype("datetime64")
+
+    category_cols = ["k", "p", "q", "t", "l"]
+    nums_cols = ["p1", "p36", "p37", "p2a", "p2b", "p6", "p7", "p8", "p9", "p10", "p11", "p12", "p13a",
+                 "p13b", "p13c", "p14", "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23", "p24", "p27", "p28",
+                 "p34", "p35", "p39", "p44", "p45a", "p47", "p48a", "p49", "p50a", "p50b", "p51", "p52", "p53", "p55a",
+                 "p57", "p58", "a", "b", "d", "e", "f", "g", "h", "i", "j", "n", "o", "r", "s", "p5a"]
+
+    new_df[category_cols] = new_df[category_cols].astype("category")
+    new_df[nums_cols] = new_df[nums_cols].apply(pd.to_numeric, errors="coerce")
+
+    if verbose:
+        print(f"orig_size={df.memory_usage(deep=True).sum() / 10**6:.1f} MB")
+        print(
+            f"new_size={new_df.memory_usage(deep=True).sum() / 10**6:.1f} MB")
+    return new_df
 
 # Ukol 3: počty nehod v jednotlivých regionech podle viditelnosti
 
 
 def plot_visibility(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
-    pass
+    """
+    Plots the number of accidents from 4 region with dependence on visibility.
+
+    :param df: pandas DataFrame
+    :param fig_location: location to save the figure
+    :param show_figure: whether to show the figure
+    """
+    visibility = {
+        1: "Viditelnost ve dne - nezhoršená",
+        2: "Viditelnost ve dne - zhoršená",
+        3: "Viditelnost ve dne - zhoršená",
+        4: "Viditelnost v noci - nezhoršená",
+        5: "Viditelnost v noci - zhoršená",
+        6: "Viditelnost v noci - nezhoršená",
+        7: "Viditelnost v noci - zhoršená",
+    }
+    selected_regions = ["OLK", "ZLK", "VYS", "PAK"]
+
+    df = df[df["region"].isin(selected_regions)].copy()
+    df["p19_visibility"] = df["p19"].map(visibility)
+
+    grouped = df.groupby(["region", "p19_visibility"]).agg(
+        {"p1": "nunique"}).reset_index()
+
+    sns.set_context("paper", font_scale=1.5)
+    sns.set_style("whitegrid")
+
+    graph = sns.catplot(x="region", y="p1", col="p19_visibility", data=grouped, kind="bar", col_wrap=2, sharey=False, col_order=[
+                        "Viditelnost ve dne - nezhoršená",  "Viditelnost ve dne - zhoršená",  "Viditelnost v noci - nezhoršená", "Viditelnost v noci - zhoršená"])
+
+    graph.set_titles("{col_name}")
+    graph.set_xlabels("Kraje")
+    graph.set_ylabels("Počet nehod")
+    graph.fig.suptitle("Počet nehod podle viditelnosti a podle dne či noci")
+    graph.fig.subplots_adjust(top=0.9)
+
+    if fig_location:
+        plt.savefig(fig_location)
+
+    if show_figure:
+        plt.show()
 
 # Ukol4: druh srážky jedoucích vozidel
 
 
 def plot_direction(df: pd.DataFrame, fig_location: str = None,
                    show_figure: bool = False):
-    pass
+    """
+    Plots the number of accidents from 4 region with dependence on direction.
+
+    :param df: pandas DataFrame
+    :param fig_location: location to save the figure
+    :param show_figure: whether to show the figure
+    """
+    direction = {
+        1: "Čelní",
+        2: "Boční",
+        3: "Boční",
+        4: "Zezadu",
+    }
+    selected_regions = ["OLK", "ZLK", "VYS", "PAK"]
+
+    df = df[df["p7"] != 0]
+    df = df[df["region"].isin(selected_regions)]
+    df["p7"] = df["p7"].map(direction)
+    df["month"] = df["date"].dt.month
+
+    grouped = df.groupby(["region", "p7", "month"]).agg(
+        {"p1": "nunique"}).reset_index()
+
+    sns.set_context("paper", font_scale=1.5)
+    sns.set_style("whitegrid")
+
+    graph = sns.catplot(x="month", y="p1", hue="p7", col="region",
+                        data=grouped, kind="bar", col_wrap=2, sharey=False)
+
+    graph.set_titles("Kraj: {col_name}")
+    graph.fig.subplots_adjust(top=0.9, left=0.1, right=0.85)
+    graph.fig.subplots_adjust(hspace=0.2, wspace=0.2)
+    graph._legend.set_title("Druh srážky")
+
+    for subplot in graph.axes.flatten():
+        subplot.tick_params(labelbottom=True)
+        subplot.set_xlabel("Měsíc")
+        subplot.set_ylabel("Počet nehod")
+
+    if fig_location:
+        plt.savefig(fig_location)
+
+    if show_figure:
+        plt.show()
 
 # Ukol 5: Následky v čase
 
 
 def plot_consequences(df: pd.DataFrame, fig_location: str = None,
                       show_figure: bool = False):
+    """
+    Plots the number of accidents from 4 region with dependence on consequences.
+
+    :param df: pandas
+    :param fig_location: location to save the figure
+    :param show_figure: whether to show the figure
+    """
     pass
 
 
@@ -86,7 +210,7 @@ if __name__ == "__main__":
     df2 = parse_data(df, True)
 
     plot_visibility(df2, "01_visibility.png")
-    plot_direction(df2, "02_direction.png", True)
+    plot_direction(df2, "02_direction.png")
     plot_consequences(df2, "03_consequences.png")
 
 
